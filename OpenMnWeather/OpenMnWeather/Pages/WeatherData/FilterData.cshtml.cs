4ebe4a7da33f7320c598c267def345a183c6a435
John Muraski <62039431+jtmuraski@users.noreply.github.com>
2022-04-19 08:25:14 -0500

    SyncFusiuon support
    Added support for the SyncFusion libraries

---
+++ OpenMnWeather/OpenMnWeather/Pages/WeatherData/FilterData.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OpenMnWeather.Pages.WeatherData
+{
+    public class FilterDataModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
